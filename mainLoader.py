f43908ea6dc8192ef2a6bb676eef9ec774cd10d7
Taewoo Han <htw1127@berkeley.edu>
2020-11-18 12:19:18 -0800

    minor bug fixed

--- mainLoader.py
+++ mainLoader.py
@@ -8,5 +8,6 @@ for name in matrix_dict.keys():
     pos_list, sub_row, sub_col = matrix_dict[name]
     matrix_realize[name] = np.ones((sub_row, sub_col)) * content
 
+## todo: turn a into a sparce matrix; readup on scipy.sparce.csc_matrix!!
 A = realize_matrix(row, col, matrix_dict, matrix_realize)
 print(A)
